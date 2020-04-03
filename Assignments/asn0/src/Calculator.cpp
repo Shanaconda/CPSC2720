bfb976f25d40b9234c17b519b72f5b28741f7b40
Shannon Abeda <58862531+Shanaconda@users.noreply.github.com>
2020-04-02 23:07:48 -0600

    Update Calculator.cpp
    Style fixing

--- Assignments/asn0/src/Calculator.cpp
+++ Assignments/asn0/src/Calculator.cpp
@@ -1,36 +1,33 @@
 #include "Calculator.h"
 #include "Exception.h"
 
-Calculator::Calculator()
-{
+Calculator::Calculator() {
   //ctor
 }
 
-Calculator::~Calculator()
-{
+Calculator::~Calculator() {
   //dtor
 }
 
-int Calculator::add(int x, int y)
-{
+int Calculator::add(int x, int y) {
 
   return x+y;
 
 }
 
-int Calculator::sub(int x, int y){
+int Calculator::sub(int x, int y) {
 
   return x-y;
 
 }
 
-int Calculator::mult(int x, int y){
+int Calculator::mult(int x, int y) {
 
     return x*y;
 
  }
 
- int Calculator::div(int x, int y){
+ int Calculator::div(int x, int y) {
   if(y==0)
     throw div_by_zero_error("Error");
     return x/y;
